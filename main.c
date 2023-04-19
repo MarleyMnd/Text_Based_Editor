85e331c5933a819678e875637669781c7de69e60
Marley <mrl.menard@gmail.com>
2023-04-19 12:39:03 +0200

    First commit

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <malloc.h>
+
+// Structure to create a point
+typedef struct {
+    int x;
+    int y;
+}POINT;
+
+// Function which creates a point using the structure
+POINT *createPoint(int x, int y) {
+    POINT *p = malloc(sizeof(POINT));
+    p->x = x;
+    p->y = y;
+    return p;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int width = 10, i, j;
+    int x, y;
+
+    for (i = 0; i < width; i++) {
+        for (j = 0; j < width; j++) {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+
+    printf("Enter two coordinates :");
+    scanf("%d %d", x, y);
+
+    POINT *p = createPoint(x, y);
+    printf("Point created at (%d, %d)", p->x, p->y);
+
     return 0;
 }
