92f3e94c4ae76f8d72c39ff87efdc53b425bae01
Marley <mrl.menard@gmail.com>
2023-04-23 10:45:12 +0200

    New .c .h files

--- functions.c
+++ functions.c
@@ -2,11 +2,10 @@
 // Created by Marley on 19/04/2023.
 //
 
+#include <stdio.h>
 #include <malloc.h>
 #include "functions.h"
 
-// Structure to create a point
-typedef struct point POINT;
 
 // Function which creates a point using the structure
 POINT *createPoint(int x, int y) {
@@ -16,9 +15,43 @@ POINT *createPoint(int x, int y) {
     return p;
 }
 
-LINE *createLine(POINT *p1, POINT *p2) {
-    LINE *l = malloc(sizeof(LINE));
-    l->p1 = p1;
-    l->p2 = p2;
-    return l;
+// Function which creates a line using the structure
+LINE *createLine(POINT *Point1, POINT *Point2) {
+    LINE *line = malloc(sizeof(LINE));
+    line->p1 = createPoint(Point1->x, Point1->y);
+    line->p2 = createPoint(Point2->x, Point2->y);
+    return line;
+}
+
+// Function to create a square with 4 lines
+SQUARE *createSquare(POINT *p1, POINT *p2, POINT *p3, POINT *p4) {
+    SQUARE *square = malloc(sizeof(SQUARE));
+    square->l1 = *createLine(p1, p2);
+    square->l2 = *createLine(p2, p3);
+    square->l3 = *createLine(p3, p4);
+    square->l4 = *createLine(p4, p1);
+    return square;
+}
+
+// Function to print a point
+void *printPoint(POINT *p) {
+    printf("Point (%d, %d)", p->x, p->y);
+}
+
+// Function to print a line
+void *printLine(LINE *line) {
+    printf("Line from ");
+    printPoint(line->p1);
+    printf(" to ");
+    printPoint(line->p2);
+    printf("\n");
+}
+
+// Function to print a square
+void *printSquare(SQUARE *square) {
+    printf("Square with lines:\n");
+    printLine(&square->l1);
+    printLine(&square->l2);
+    printLine(&square->l3);
+    printLine(&square->l4);
 }
--- main.c
+++ main.c
@@ -12,7 +12,8 @@ int main() {
     printf("Choose an option:\n");
     printf("1. Create a point\n");
     printf("2. Create a line\n");
-    printf("3. Exit\n");
+    printf("3. Create a square\n");
+    printf("10. Exit\n");
 
     int option;
     printf("Enter your choice:");
@@ -20,22 +21,48 @@ int main() {
 
     switch(option) {
         case 1:
-            printf("Enter two coordinates :");
-            scanf("%d %d", x, y);
+            printf("Enter two coordinates (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
             POINT *p = createPoint(x, y);
-            printf("Point created at (%d, %d)", p->x, p->y);
+
+            printPoint(p);
             break;
         case 2:
-            printf("Enter two coordinates for point 1:");
-            scanf("%d %d", x, y);
+            printf("Enter two coordinates for point 1 (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
             POINT *p1 = createPoint(x, y);
-            printf("Enter two coordinates for point 2:");
-            scanf("%d %d", x, y);
+
+            printf("Enter two coordinates for point 2 (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
             POINT *p2 = createPoint(x, y);
+
             LINE *l = createLine(p1, p2);
-            printf("Line created between (%d, %d) and (%d, %d)", l->p1->x, l->p1->y, l->p2->x, l->p2->y);
+
+            printLine(l);
             break;
         case 3:
+            printf("Enter two coordinates for point 1 (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
+            POINT *p3 = createPoint(x, y);
+
+            printf("Enter two coordinates for point 2 (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
+            POINT *p4 = createPoint(x, y);
+
+            printf("Enter two coordinates for point 3 (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
+            POINT *p5 = createPoint(x, y);
+
+            printf("Enter two coordinates for point 4 (format: \033[0;31mA B\033[0;0m):");
+            scanf("%d %d", &x, &y);
+            POINT *p6 = createPoint(x, y);
+
+            SQUARE *s = createSquare(p3, p4, p5, p6);
+
+            printSquare(s);
+            break;
+        case 10:
+            printf("Exiting...");
             break;
         default:
             printf("Invalid option");
